e3c788f11a1e33eea6ecb6fa00c7b07615578101
DioExtreme <diodotexe@gmail.com>
2021-12-17 12:02:22 +0200

    Remove unneeded BZip2 code

--- BZip2/BZip2Exception.cs
+++ BZip2/BZip2Exception.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace Tunetoon.BZip2
 {
@@ -10,44 +9,11 @@ namespace Tunetoon.BZip2
 	public class BZip2Exception : Exception
 	{
 		/// <summary>
-		/// Initialise a new instance of <see cref="BZip2Exception" />.
-		/// </summary>
-		public BZip2Exception()
-		{
-		}
-
-		/// <summary>
 		/// Initialise a new instance of <see cref="BZip2Exception" /> with its message string.
 		/// </summary>
 		/// <param name="message">A <see cref="string"/> that describes the error.</param>
 		public BZip2Exception(string message)
 			: base(message)
-		{
-		}
-
-		/// <summary>
-		/// Initialise a new instance of <see cref="BZip2Exception" />.
-		/// </summary>
-		/// <param name="message">A <see cref="string"/> that describes the error.</param>
-		/// <param name="innerException">The <see cref="Exception"/> that caused this exception.</param>
-		public BZip2Exception(string message, Exception innerException)
-			: base(message, innerException)
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the BZip2Exception class with serialized data.
-		/// </summary>
-		/// <param name="info">
-		/// The System.Runtime.Serialization.SerializationInfo that holds the serialized
-		/// object data about the exception being thrown.
-		/// </param>
-		/// <param name="context">
-		/// The System.Runtime.Serialization.StreamingContext that contains contextual information
-		/// about the source or destination.
-		/// </param>
-		protected BZip2Exception(SerializationInfo info, StreamingContext context)
-			: base(info, context)
 		{
 		}
 	}
