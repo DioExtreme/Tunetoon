b1d08d78d76b863899d52638a31bc5a517508bae
DioExtreme <diodotexe@gmail.com>
2021-12-19 12:25:56 +0200

    Refactor Rewritten's login process
    - Fixes infrequent bugs
    - Two-Step window is centered to the main one

--- Login/RewrittenLoginHandler.cs
+++ Login/RewrittenLoginHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Tunetoon.Accounts;
 using Tunetoon.Forms;
 
@@ -13,41 +14,51 @@ namespace Tunetoon.Login
         private const string RewrittenBaseAddress = "https://www.toontownrewritten.com";
 
         // See: https://github.com/ToontownRewritten/api-doc/blob/master/login.md
-        public override void GetAuthResponse(RewrittenAccount account)
+
+        private void GetAndHandleLoginResult(RewrittenAccount account, Dictionary<string, string> data)
         {
-            var result = account.LoginResult;
-            var status = account.LoginResult.Success;
-
-            var data = new Dictionary<string, string>();
-
             using (var request = new HttpRequestMessage(HttpMethod.Post, RewrittenBaseAddress + "/api/login?format=json"))
             {
-                if (status == null || status == "true" || status == "false")
-                {
-                    data.Add("username", account.Username);
-                    data.Add("password", account.Password);
-                }
-                else if (status == "partial")
-                {
-                    data.Add("appToken", result.AuthToken);
-                    data.Add("authToken", result.ResponseToken);
-                }
-                else if (status == "delayed")
-                {
-                    data.Add("queueToken", result.QueueToken);
-                }
-                else
-                {
-                    return;
-                }
-
                 request.Content = new FormUrlEncodedContent(data);
                 var response = httpClient.SendAsync(request).Result;
                 string responseString = response.Content.ReadAsStringAsync().Result;
                 account.LoginResult = JsonConvert.DeserializeObject<RewrittenLoginResult>(responseString);
             }
-
             HandleAuthResponse(account);
+        }
+
+        public override void GetAuthResponse(RewrittenAccount account)
+        {
+            var data = new Dictionary<string, string>();
+
+            data.Add("username", account.Username);
+            data.Add("password", account.Password);
+
+            GetAndHandleLoginResult(account, data);
+        }
+
+        private void GetAuthResponseAfterPartial(RewrittenAccount account)
+        {
+            var result = account.LoginResult;
+            var status = result.Success;
+
+            var data = new Dictionary<string, string>();
+
+            if (status == "partial")
+            {
+                data.Add("appToken", result.AuthToken);
+                data.Add("authToken", result.ResponseToken);
+            }
+            else if (status == "delayed")
+            {
+                data.Add("queueToken", result.QueueToken);
+            }
+            else
+            {
+                return;
+            }
+
+            GetAndHandleLoginResult(account, data);
         }
 
         public override void HandleAuthResponse(RewrittenAccount account)
@@ -72,23 +83,27 @@ namespace Tunetoon.Login
                 AccountsToTwoStepAuth.Add(account);
                 return;
             }
-            GetAuthResponse(account);
+
+            GetAuthResponseAfterPartial(account);
+        }
+
+        private bool NoTwoStepTokenGiven(string authToken)
+        {
+            return authToken == null || authToken.Equals("ToonGuard") || authToken.Equals("Two-Step Token");
         }
 
         private async Task RequestLoginAfterTwoStep()
         {
             var tasks = new List<Task>();
-            var authList = new List<RewrittenAccount>(AccountsToTwoStepAuth);
-
-            foreach (var account in authList)
+            foreach (var account in AccountsToTwoStepAuth.ToArray())
             {
-                string authCode = account.LoginResult.AuthToken;
-                if (authCode == null || authCode.Equals("ToonGuard") || authCode.Equals("Two-Step Token"))
+                string authToken = account.LoginResult.AuthToken;
+                if (NoTwoStepTokenGiven(authToken))
                 {
                     continue;
                 }
                 AccountsToTwoStepAuth.Remove(account);
-                tasks.Add(Task.Run(() => GetAuthResponse(account)));
+                tasks.Add(Task.Run(() => GetAuthResponseAfterPartial(account)));
             }
             await Task.WhenAll(tasks);
         }
@@ -101,6 +116,7 @@ namespace Tunetoon.Login
                 var authForm = new Auth(AccountsToTwoStepAuth);
                 authForm.AuthTokensEntered += (accountsToAuth) => AccountsToTwoStepAuth = accountsToAuth;
                 authForm.IsClosed += () => authCancelled = true;
+                authForm.StartPosition = FormStartPosition.CenterParent;
                 authForm.ShowDialog();
 
                 if (authCancelled)
