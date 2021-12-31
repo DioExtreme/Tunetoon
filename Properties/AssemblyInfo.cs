8050de29c34d0cab2644e41fb71fe569e2051084
DioExtreme <diodotexe@gmail.com>
2021-12-31 18:59:46 +0200

    v1.7.0.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Null / DioExtreme")]
 [assembly: AssemblyProduct("Tunetoon")]
-[assembly: AssemblyCopyright("Copyright © 2017-2021 Null / DioExtreme")]
+[assembly: AssemblyCopyright("Copyright © 2017-2022 Null / DioExtreme")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.4")]
-[assembly: AssemblyFileVersion("1.6.0.4")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
