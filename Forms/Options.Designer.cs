3d03bb412aec00a2ffd987b6d46583e548b305e8
DioExtreme <diodotexe@gmail.com>
2022-10-20 00:24:19 +0300

    Just a tiny bit more space

--- Forms/Options.Designer.cs
+++ Forms/Options.Designer.cs
@@ -148,7 +148,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(508, 200);
+            this.ClientSize = new System.Drawing.Size(508, 203);
             this.Controls.Add(this.EncryptAccsCheckBox);
             this.Controls.Add(this.SkipUpdatesCheckBox);
             this.Controls.Add(this.ClashPathButton);
